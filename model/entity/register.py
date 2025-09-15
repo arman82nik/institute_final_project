bed89ea26f413b935770772d804dfde4d07ce00f
Asus <mohsennik1352@gmail.com>
2025-09-15 15:45:29 +0330

    upload 1

--- model/entity/register.py
+++ model/entity/register.py
@@ -48,17 +48,17 @@ class Register(Base):
 
 
 
-class Register:
-    def __init__(self,id,person_id,name,family,phone_number,curse_number):
-        self.id = id
-        self.person_id = person_id
-        self.name = name
-        self.family = family
-        self.phone_number = phone_number
-        self.curse_number = curse_number
+#class Register:
+    #def __init__(self,id,person_id,name,family,phone_number,curse_number):
+        #self.id = id
+        #self.person_id = person_id
+        #self.name = name
+        #self.family = family
+        #self.phone_number = phone_number
+        #self.curse_number = curse_number
 
-    def __repr__(self):
-        return f"{self.__dict__}"
+    #def __repr__(self):
+        #return f"{self.__dict__}"
 
-    def to_tuple(self):
-        return tuple((self.id, self.person_id, self.name, self.family, self.phone_number, self.curse_number))
+    #def to_tuple(self):
+       # return tuple((self.id, self.person_id, self.name, self.family, self.phone_number, self.curse_number))
