67e3f528e99d89f67282fd09ff21eb442e5b1d67
Asus <mohsennik1352@gmail.com>
2025-09-15 15:54:09 +0330

    uploaded 2

--- model/entity/student.py
+++ model/entity/student.py
@@ -1,18 +1,25 @@
-class Student:
-    def __init__(self,id,person_id,name,family,birthday,email,gender):
-        self.id = id
+from sqlalchemy import  Integer, String, Column
+from model.entity.base import Base
+
+class Student(Base):
+    __tablename__ = "student"
+
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer,nullable=False)
+    name = Column(String(30),nullable=False)
+    age = Column(Integer,nullable=False)
+    gender = Column(String(250),nullable=False)
+    birthday = Column(String)
+    email = Column(String)
+
+    def __init__(self, person_id, name, age, gender, birthday, email):
         self.person_id = person_id
         self.name = name
-        self.family = family
+        self.age = age
+        self.gender = gender
         self.birthday = birthday
         self.email = email
-        self.gender = gender
+
 
     def __repr__(self):
         return f"{self.__dict__}"
-
-
-    def to_tuple(self):
-        return tuple([self.id,self.name,self.family,self.birthday,self.email,self.gender])
-
-
