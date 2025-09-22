88ff60939d1f12c321dff25999d1381c6fd701df
AsusIran <prezmaster1995@gmail.com>
2025-09-23 00:10:06 +0330

    exercise sevice,controller and test done 31/6

---
+++ test/exercise_test.py
@@ -0,0 +1,20 @@
+from controller.exercise_controller import ExerciseController
+from test.student_test import test
+
+exercise_controller = ExerciseController()
+
+# save test []
+# print(exercise_controller.save(1, 1, 1, 18))
+#
+# edit test []
+# print(exercise_controller.edit(1, 1, 1, 2,20))
+#
+# delete test []
+# print(exercise_controller.delete(1))
+#
+# find all test []
+# print(exercise_controller.find_all())
+#
+# find by id test []
+# print(exercise_controller.find_by_id(1))
+
